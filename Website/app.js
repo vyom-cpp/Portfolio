8d00c68d7c0c7ba06bd6eac58fb3fc10e0c6e74c
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-01-29 16:25:43 +0530

    Done some changes

---
+++ Website/app.js
@@ -0,0 +1,7 @@
+// Toggle mobile navigation
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
